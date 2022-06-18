025e7c621aa1c77228ced4e83be373e922686921
Anurag Shukla <tachyonanurag@gmail.com>
2022-06-18 22:54:52 +0530

    ...

--- client/client.js
+++ client/client.js
@@ -1,20 +1,39 @@
 var socket = require('socket.io-client')('https://nodereplchat.herokuapp.com/');
-  const repl = require('repl')
-  const chalk = require('chalk');
+const repl = require('repl')
+const chalk = require('chalk');
 const username = "anon"
-  socket.on('disconnect', function() {
-      socket.emit('disconnect')
-  });
-  socket.on('connect', () => {
-      console.log(chalk.red('=== start chatting ==='))
-  })
-  socket.on('message', (data) => {
-      const { cmd, username } = data
-      console.log(chalk.green(username + ': ' + cmd.split('\n')[0]));
-  })
-  repl.start({
-      prompt: '',
-	  eval: (cmd) => {
-		  socket.send({cmd,username})
-      }
-  })
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+socket.on('disconnect', function() {
+	socket.emit('disconnect')
+});
+socket.on('connect', () => {
+	console.log(chalk.red('=== start chatting ==='))
+})
+socket.on('message', (data) => {
+	const { msg, username } = data
+	console.log(chalk.green(username + ': ' + msg.split('\n')[0]));
+})
+/*
+repl.start({
+	prompt: '',
+	eval: (cmd) => {
+		socket.send({cmd,username})
+	}
+})*/
+
+
+const inpMsg = (ques,cb)=>{
+	readline.question(ques,(msg)=>{
+		cb(msg)
+	})
+}
+const printMsg = (msg) =>{
+	socket.emit('message',{msg,username})
+	inpMsg('# ',printMsg)
+}
+
+inpMsg('# ',printMsg)
+

3dd624b0429847bc12a6b66925136a6ca2d12063
Anurag Shukla <tachyonanurag@gmail.com>
2022-06-27 13:00:04 +0530

    lot of changes

--- client/client.js
+++ client/client.js
@@ -1,39 +1,107 @@
-var socket = require('socket.io-client')('https://nodereplchat.herokuapp.com/');
-const repl = require('repl')
+const url = 'http://localhost:3000' || 'https://nodereplchat.herokuapp.com/'
 const chalk = require('chalk');
-const username = "anon"
+const user = process.argv[2]
+const room = process.argv[3]
+const figlet = require('figlet')
+let logo = false
+let loading=true
+let socket
+const rl = require('readline')
 const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout
 })
-socket.on('disconnect', function() {
-	socket.emit('disconnect')
-});
-socket.on('connect', () => {
-	console.log(chalk.red('=== start chatting ==='))
-})
-socket.on('message', (data) => {
-	const { msg, username } = data
-	console.log(chalk.green(username + ': ' + msg.split('\n')[0]));
-})
-/*
-repl.start({
-	prompt: '',
-	eval: (cmd) => {
-		socket.send({cmd,username})
-	}
-})*/
-
-
+const color = ['red', 'green', 'blue', 'magenta', 'cyan', 'white'];
 const inpMsg = (ques,cb)=>{
 	readline.question(ques,(msg)=>{
+		rl.moveCursor(process.stdout,0,-2)
+		rl.clearScreenDown(process.stdout)
 		cb(msg)
 	})
 }
 const printMsg = (msg) =>{
-	socket.emit('message',{msg,username})
+	socket.emit('message',{msg,user})
 	inpMsg('# ',printMsg)
 }
+const log = (msg,newLine) => {
+	if(newLine)
+		process.stdout.write("\n")
+	process.stdout.write(msg)
+}
+const clrL = () => {
+	process.stdout.write("\r\x1b[K")
+}
+const connecting =async ()=>{
+	while(loading){
+		log("connecting.")
+		await sleep(250)
+		clrL()
+		log("connecting..")
+		await sleep(250)
+		clrL()
+		log("connecting...")
+		await sleep(250)
+		clrL()
+	}
+	if(!loading){
+		clrL()
+		console.log("connected to server!")
+	}
+}
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-inpMsg('# ',printMsg)
+const wait = async () => {
+	if(logo){
+		return
+	}
+	await sleep(250)
+	return wait()
+}
 
+const printIncoming = (msg,usr,clr,uid)=>{
+	rl.moveCursor(process.stdout, 0, -2)
+	clrL()
+	log(chalk[color[clr]](`${usr}#${uid}` + ': ' + msg.split('\n')[0])+'\n')
+	rl.clearScreenDown(process.stdout)
+	console.log('\n')
+	inpMsg('# ',printMsg)
+
+}
+
+const startup = async()=>{
+	figlet("REPL CHAT",async (err,data)=>{
+		console.log(data)
+		logo=true
+	})
+	await wait()
+	connecting()
+	socket = require('socket.io-client')(url)
+	socket.emit('usrInfo',user,room)
+	socket.on('connect', async () => {
+		if(user==null || user==undefined || user.length<=3){
+			console.log(chalk.red("\n\nsomething is wrong! please restart the program."))
+			process.exit(1)
+		}else{
+			loading=false
+		}
+		await sleep(1000)
+		console.log('\n\n\n')
+		inpMsg('# ',printMsg)
+
+		socket.on('disconnect', ()=> {
+			socket.emit('disconnect')
+		})
+		socket.on('message', (data,clr,uid) => {
+			const { msg, user } = data
+			printIncoming(msg,user,clr,uid)
+		})
+		socket.on('yourMsg',(data,clr,uid)=>{
+			const { msg} = data
+			printIncoming(msg,"you",clr,uid)
+
+		})
+	})
+
+}
+
+startup()	
--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const port = process.env.PORT || 3000
+
 /* Middlewares	  */
 
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({
 	extended: true
-}));
+}))
 
 /* server stuff	  */
 
@@ -16,15 +17,25 @@ app.get('/',(req,res)=>{
 })
 
 const server = http.listen(port, () => {
-	console.log(`listening on http://localhost:${port}/`)
-});
+	console.log(`listening on ${port}`)
+})
 
 /* socket.io stuff  */
-const io = require('socket.io')(server)
-io.on('connection',(socket)=>{
 
+const io = require('socket.io')(server)
+io.on('connection',async (socket)=>{
+	socket.on('usrInfo',async (user,room)=>{
+		socket.data.user = user
+		socket.data.room = room
+		const id = 10000000 +  Math.floor(Math.random()*9000000)
+		socket.data.id = id
+		socket.data.color = id%6
+		await socket.join(room)
+	})
 	socket.on('message',(data)=>{
-		socket.broadcast.emit('message',data)
+		console.log(socket.data.id)
+		socket.to(socket.data.room).emit('message',data,socket.data.color,Math.floor(socket.data.id/10000))
+		socket.emit('yourMsg',data,socket.data.color,Math.floor(socket.data.id/10000))
 	})
 })
 
